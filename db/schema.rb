8d1a2ded197bfff4acf4a27a65dd5905ce2fa3de
jspencerhopkins <jspencerhopkins@gmail.com>
2015-02-17 14:51:02 -0500

    Setup users and sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217181908) do
+ActiveRecord::Schema.define(version: 20150217193357) do
 
   create_table "comerrs", force: :cascade do |t|
     t.string   "message"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20150217181908) do
     t.string   "resolution"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
