fbf46ea0dc20427995ed6a8987586570c326ebcc
jspencerhopkins <jspencerhopkins@gmail.com>
2015-02-17 21:37:53 -0500

    Add validation to form for new errors

--- app/models/comerr.rb
+++ app/models/comerr.rb
@@ -1,6 +1,8 @@
 class Comerr < ActiveRecord::Base
 	belongs_to :user
 
+	validates :message, :description, :resolution, presence: true
+
 	has_attached_file :image, :styles => { :medium => "800x10000>", 
 																					:thumb => "300x500>" }, 
 																					:default_url => "/images/:style/missing.png"
